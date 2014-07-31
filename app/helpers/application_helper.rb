590fac3794e3db5f3d7605653c5468fe8f6d61b0
Paul Cantrell <cantrell@pobox.com>
2014-07-30 19:41:21 -0500

    Style somewhat restored; parallax; fonts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def hero(&block)
+    @hero_count ||= 0
+    @hero_count += 1
+    content_tag(
+      :div,
+      capture(&block),
+      class: 'hero',
+      'data-stellar-background-ratio' => 0.5,
+      'data-stellar-vertical-offset' => @hero_count * 300)
+  end
 end
