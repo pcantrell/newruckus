741e19737b6a13b309fa68399c20c4088f6c1d6c
Paul Cantrell <cantrell@pobox.com>
2014-08-11 12:17:37 -0500

    Admin dashboard showing signups

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -10,24 +10,40 @@ ActiveAdmin.register_page "Dashboard" do
       end
     end
 
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
+    columns do
+      column do
+        panel 'Composer Night Queue' do
+          ul do
+            ComposerNightSignup.queue.includes(:presenter).each do |signup|
+              li(class: 'signup') do
+                span link_to(signup.presenter.name, edit_admin_composer_night_signup_path(signup))
+                span signup.comments, class: 'notes'
+                span signup.internal_notes, class: 'notes internal'
+              end
+            end
+          end
+        end
+      end
+      column do
+        panel 'Upcoming Composer Nights' do
+          ComposerNight.upcoming.each do |event|
+            para do
+              b do
+                link_to event.title, admin_composer_night_path(event)
+              end
+            end
 
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+            ul do
+              event.signups.includes(:presenter).each do |signup|
+                li do
+                  link_to signup.presenter.name, edit_admin_composer_night_signup_path(signup)
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+
+  end
 end
