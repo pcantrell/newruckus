7ecf7bf200ffd6a27b89f8abfd30a68adc2ef93e
Paul Cantrell <cantrell@pobox.com>
2014-10-14 10:13:27 -0500

    Copy Katherine with performer info

--- app/mailers/admin_notifications.rb
+++ app/mailers/admin_notifications.rb
@@ -5,7 +5,11 @@ class AdminNotifications < ActionMailer::Base
     @signup = signup
     @changed_attrs = changed_attrs
 
+    to = ["paul@innig.net"]
+    to << "katherine@katherinebergman.net" if signup.scheduled?
+
     mail from: signup.presenter.email,
+         to: to,
          subject: "Composer Night #{new_signup ? 'new signup' : 'info changed'}: #{signup.presenter.name}"
   end
 end
